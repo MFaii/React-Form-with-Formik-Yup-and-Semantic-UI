647e4dada358a325bb1324270b3e35f8ae4bcddb
Maximiliano Failla <faillamaximiliano@gmail.com>
2023-01-17 21:09:41 -0300

    Translate

--- src/App.js
+++ src/App.js
@@ -12,16 +12,16 @@ function App() {
       repeatpassword: "",
     },
     validationSchema: Yup.object({
-      name: Yup.string().required("Name requerido"),
+      name: Yup.string().required("Name and surname required"),
       email: Yup.string()
-        .email("No es un email valido")
-        .required("Email requerido"),
+        .email("It is not a valid email")
+        .required("Email required"),
       password: Yup.string()
-        .required("Psw Requerida")
-        .oneOf([Yup.ref("repeatpassword")], "Las psw no coinciden"),
+        .required("Password required")
+        .oneOf([Yup.ref("repeatpassword")], "Passwords do not match"),
       repeatpassword: Yup.string()
-        .required("Psw Requerida")
-        .oneOf([Yup.ref("password")], "Las psw no coinciden"),
+        .required("Password required")
+        .oneOf([Yup.ref("password")], "Passwords do not match"),
     }),
     onSubmit: (formData) => {
       console.log(formData);
@@ -39,11 +39,11 @@ function App() {
         height: "100vh",
       }}
     >
-      <h1>Formulario de registro</h1>
+      <h1>Registration Form</h1>
       <Form style={{ width: "30%" }} onSubmit={formik.handleSubmit}>
         <Form.Input
           type="text"
-          placeholder="Nombre y apellido"
+          placeholder="Name and surname"
           name="name"
           onChange={formik.handleChange}
           error={formik.errors.name}
@@ -67,15 +67,15 @@ function App() {
         />
         <Form.Input
           type="password"
-          placeholder="repeatpassword"
+          placeholder="Repeat Password"
           name="repeatpassword"
           onChange={formik.handleChange}
           error={formik.errors.repeatpassword}
           value={formik.values.repeatpassword}
         />
-        <Button type="submit">Registro</Button>
+        <Button type="submit">Registry</Button>
         <Button type="button" onClick={formik.handleReset}>
-          Limpiar Formulario
+          Clean form
         </Button>
       </Form>
     </Container>
